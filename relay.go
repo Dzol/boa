2987d0573da2cfc7662768dc2cfaedd065e958e0
Joseph Yiasemides <joseph.yiasemides@gmail.com>
2018-10-04 20:30:39 +0200

    Move a lot out of main's scope into file scope to ease readability

--- relay.go
+++ relay.go
@@ -3,44 +3,12 @@ package main
 import "fmt"
 import "sync"
 
+var wg sync.WaitGroup
+
+var c = make(chan uint)
+var d = make(chan uint)
+
 func main() {
-
-	var wg sync.WaitGroup
-
-	var ping func ()
-	var pong func ()
-
-	c := make(chan uint)
-	d := make(chan uint)
-
-	ping = func () {
-		var n uint
-		defer wg.Done()
-		for {
-			n = <- c
-			fmt.Println("ping: ", n)
-			if n < 10 {
-				n++
-				d <- n
-			} else {
-				close(d)
-				break
-			}
-		}
-	}
-
-	pong = func () {
-		defer wg.Done()
-		for {
-			if n, ok := <- d; ok {
-				fmt.Println("pong: ", n)
-				n++
-				c <- n
-			} else {
-				break
-			}
-		}
-	}
 
 	wg.Add(2)
 
@@ -50,4 +18,33 @@ func main() {
 	c <- 0
 
 	wg.Wait()
+}
+
+func ping () {
+	var n uint
+	defer wg.Done()
+	for {
+		n = <- c
+		fmt.Println("ping: ", n)
+		if n < 10 {
+			n++
+			d <- n
+		} else {
+			close(d)
+			break
+		}
+	}
+}
+
+func pong() {
+	defer wg.Done()
+	for {
+		if n, ok := <- d; ok {
+			fmt.Println("pong: ", n)
+			n++
+			c <- n
+		} else {
+			break
+		}
+	}
 }
